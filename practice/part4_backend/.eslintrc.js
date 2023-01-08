e1c4336d9e6579a2e0fc7d1fa7dc2407b9587495
Tony Guo <tgo@TonyPC2019.localdomain>
2023-01-09 01:29:54 +0800

    part4a: Testing Node application

--- practice/part4_backend/.eslintrc.js
+++ practice/part4_backend/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    jest:true,
   },
   extends: 'eslint:recommended',
   overrides: [],
@@ -11,7 +12,7 @@ module.exports = {
   },
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     eqeqeq: 'error',
