15c9a85a2cefb87bb9529311d7a79c3c19bf8c30
tonyguo0 <tonyguo998@gmail.com>
2023-01-14 16:52:02 +0800

    part4b: Test environment, added test databases in .env and the logic to use the test databases

--- exercises/bloglist/utils/config.js
+++ exercises/bloglist/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = process.env.MONGODB_URI_BLOG
 const PORT = process.env.PORT
 
 module.exports = {
--- practice/part4_backend/utils/config.js
+++ practice/part4_backend/utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI_NOTE
+    : process.env.MONGODB_URI_NOTE
 
 module.exports = {
   MONGODB_URI,
