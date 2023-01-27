c73219a14f35bd03154069a29f0dacadf10cf194
tonyguo0 <tonyguo998@gmail.com>
2023-01-27 23:45:18 +0800

    Mongoose schema for users done, Creating users halfway

--- practice/part4_backend/models/note.js
+++ practice/part4_backend/models/note.js
@@ -4,6 +4,10 @@ const noteSchema = new mongoose.Schema({
   content: { type: String, minLength: 5, required: true },
   date: { type: Date, required: true },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 })
 
 noteSchema.set('toJSON', {
