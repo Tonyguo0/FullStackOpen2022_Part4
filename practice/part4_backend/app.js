b87cbecfcba85e07b3962b6f482c5b0cf05a6d2f
tonyguo0 <tonyguo998@gmail.com>
2023-02-08 21:56:56 +0800

    part4d: intro of Token authentication complete, added token generation based on the username and password and matching it with the database and finish testing it

--- practice/part4_backend/app.js
+++ practice/part4_backend/app.js
@@ -11,6 +11,7 @@ const mongoose = require('mongoose')
 
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 logger.info('connecting to MongoDB')
@@ -31,6 +32,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
